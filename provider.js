fe05fc70acbca26a35cdbc4792e218291b86b004
Nick Williams <nickwilliams85@gmail.com>
2013-11-28 14:56:37 +0000

    Add tests for standalone usage

--- provider.js
+++ provider.js
@@ -1,20 +1,119 @@
+// Provider.js 0.2
+// (c) 2013 Nick Williams
+// Provider may be freely distributed under the MIT license
+
 (function() {
 
 	// global on the server, window in the browser
 	var root = this;
 
-	// _extend() - adapted from `_.extend()` from underscore.js; minor tweak (use `Array.forEach`)
-	var _extend = function(obj) {
-	  Array.prototype.slice.call(arguments, 1).forEach(function(source) {
-	    if ( source ) {
-	      for ( var prop in source ) { obj[prop] = source[prop]; }
-	    }
-	  });
-	  return obj;
+	// Widget
+	// ------
+
+	function Widget() {
+		var deps = depsFromArgs( this, arguments );
+		this.initialize.apply( this, deps );
+	}
+
+	Widget.prototype.initialize = function(options) {};
+
+	Widget.extend = function(protoProps, staticProps) {
+		annotate( protoProps.initialize );
+		return extend.call( this, protoProps, staticProps );
+	}
+
+	// Provider
+	// --------
+
+	var Provider = {
+		cache: [],
+		provide: function(serviceName, fn) {
+			this.cache[ serviceName ] = fn;
+		},
+		provideMultiple: function(hash) {
+			for (var serviceName in hash) {
+				this.cache[ serviceName ] = hash[ serviceName ];
+			}
+		}
 	};
 
-	// `extend()` from Backbone.js
-	// plus annotation of constructors
+	// `depsFromArgs()` - The magic that happens when a constructor is called
+	// Grab the list of *annotated* parameter names from the `initialize` function. For each parameter
+	// either lookup from the appropriate provider, or override with the argument passed in
+
+	function depsFromArgs(context, args) {
+		var paramNames = context.initialize.$inject;
+
+		// If `extend` hasn't been called (no injection)
+		// or more arguments have been passed in than the injectables, just pass them right through
+		if ( !paramNames || paramNames.length < args.length ) return Array.prototype.slice.apply( args );
+
+		// `options` is magic. if it is anywhere in paramNames, it will get passed to the provider fn call
+		var optsPosition = paramNames.indexOf('options');
+		var options = optsPosition !== -1 && (optsPosition < args.length) ? args[optsPosition] : null;
+
+		var deps = [];
+		paramNames.forEach(function(paramName, i) {
+			if ( i < args.length ) {
+				deps.push( args[i] );
+				return;
+			}
+
+			// Lookup the provider for this parameter
+			if ( Provider.cache[paramName] ) {
+				deps.push( Provider.cache[paramName](options) );
+			} else {
+				deps.push( null );
+			}
+		});
+		return deps;
+	}
+
+	// Infect Backbone's `Model`, `View` and `Collection` objects
+
+	function infect() {
+		if ( !Backbone ) {
+			console.log("Provider: Couldn't infect(). No reference to Backbone found");
+			return;
+		}
+
+		['View', 'Model', 'Collection'].forEach(function(Type) {
+
+			// Save a reference to the constructor.
+			var Constructor = Backbone[ Type ];
+
+			// Monkey-patch the constructor to figure out what to inject
+			Backbone[ Type ] = function() {
+				var argsPlusInjectedArgs = depsFromArgs( this, arguments );
+
+				Constructor.apply( this, argsPlusInjectedArgs );
+			};
+
+			// Copy over the prototype
+			Backbone[ Type ].prototype = Constructor.prototype;
+
+			// Monkey-patch `extend()`
+			Backbone[ Type ].extend = function(protoProps, staticProps) {
+				annotate( protoProps.initialize );
+				return Constructor.extend.call( this, protoProps, staticProps );
+			};
+
+		});
+	}
+
+	Provider.Object = Widget;
+	Provider.infect = infect;
+
+	// Helpers / 3rd Party
+	// -------------------
+
+	// `extend()` from Backbone.js - not required in the 'backbone-only-build'. License:
+	//
+	// (c) 2010-2011 Jeremy Ashkenas, DocumentCloud Inc.
+	// (c) 2011-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+	// Backbone may be freely distributed under the MIT license.
+	// For all details and documentation:
+	// http://backbonejs.org
 	var extend = function(protoProps, staticProps) {
 		var parent = this;
 		var child;
@@ -27,10 +126,6 @@
 		} else {
 			child = function() { return parent.apply(this, arguments); };
 		}
-
-		// Annotate
-		annotate( protoProps.initialize )
-		console.log( "Constructor has been annotated with its params:", protoProps.initialize.$inject )
 
 		// Add static properties to the constructor function, if supplied.
 		_extend( child, parent, staticProps );
@@ -50,6 +145,20 @@
 		return child;
 	};
 
+	var _extend = function(obj) {
+	  Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+	    if ( source ) {
+	      for ( var prop in source ) { obj[prop] = source[prop]; }
+	    }
+	  });
+	  return obj;
+	};
+
+	// Adapted version of `annotate()` from Angular.js. License:
+	//
+	// @license AngularJS v1.2.3
+	// (c) 2010-2014 Google, Inc. http://angularjs.org
+	// License: MIT
 	var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
 	var FN_ARG_SPLIT = /,/;
 	var FN_ARG = /^\s*(_?)(.+?)\1\s*$/;
@@ -73,136 +182,6 @@
 	  return $inject;
 	};
 
-	// Widget
-	// ------
-	// Get all argument names previously annotated within the Obj's constructor, e.g. '$pager', '$dataStore'
-	// TODO: Error if no '$inject'?
-	function Widget() {
-		var deps = depsFromArgs( this, arguments )
-		this.initialize.apply( this, deps );
-	}
-
-	function depsFromArgs(context, args) {
-		var paramNames = context.initialize.$inject;
-
-		// If `extend` hasn't been called (no injection)
-		// or more arguments have been passed in than the injectables, just pass them right through
-		if ( !paramNames || paramNames.length < args.length ) return Array.prototype.slice.apply( args );
-
-		// `options` is magic. if it is anywhere in paramNames, it will get passed to the provider fn call
-		var optsPosition = paramNames.indexOf('options');
-		var options = optsPosition !== -1 && (optsPosition < args.length) ? args[optsPosition] : null;
-
-		var deps = [];
-		paramNames.forEach(function(paramName, i) {
-			if ( i < args.length ) {
-				deps.push( args[i] );
-				return;
-			}
-
-			if ( Provider.cache[paramName] ) {
-				deps.push( Provider.cache[paramName](options) );
-			} else {
-				deps.push( null );
-				console.log( 'WARNING: No provider for param:', paramName );
-			}
-		});
-		return deps;
-	}
-
-	Widget.prototype.initialize = function(options) {};
-	Widget.extend = extend;
-
-	// Provider
-	// --------
-
-	var Provider = {
-		cache: [],
-		provide: function(serviceName, fn) {
-			this.cache[ serviceName ] = fn;
-		},
-		provideMultiple: function(hash) {
-			for (var serviceName in hash) {
-				this.cache[ serviceName ] = hash[serviceName];
-			}
-		}
-	};
-
-	// Infect Backbone's `Model`, `View` and `Collection` objects
-
-	function infect() {
-		if ( !Backbone ) {
-			console.log("Provider: Couldn't infect(). No reference to Backbone found");
-			return;
-		}
-
-		// Save a reference to the constructor.
-		var ModelConstructor = Backbone.Model;
-
-		Backbone.Model = function(attributes, options) {
-			var argsPlusInjectedArgs = depsFromArgs(this, arguments);
-
-			// Act like nothing happened, eh tbranyen.
-			ModelConstructor.apply(this, argsPlusInjectedArgs);
-		};
-
-		// Copy over the prototype as well.
-		Backbone.Model.prototype = ModelConstructor.prototype;
-
-		// Monkey-patch `extend()`
-		Backbone.Model.extend = function(protoProps, staticProps) {
-			annotate(protoProps.initialize);
-			return ModelConstructor.extend.call(this, protoProps, staticProps);
-		};
-
-		///////
-
-		// Save a reference to the constructor.
-		var ViewConstructor = Backbone.View;
-
-		Backbone.View = function(options) {
-			var argsPlusInjectedArgs = depsFromArgs(this, arguments);
-
-			// Act like nothing happened, eh tbranyen.
-			ViewConstructor.apply(this, argsPlusInjectedArgs);
-		};
-
-		// Copy over the prototype as well.
-		Backbone.View.prototype = ViewConstructor.prototype;
-
-		// Monkey-patch `extend()`
-		Backbone.View.extend = function(protoProps, staticProps) {
-			annotate(protoProps.initialize);
-			return ViewConstructor.extend.call(this, protoProps, staticProps);
-		};
-
-		/////////
-
-		var CollectionConstructor = Backbone.Collection;
-
-		Backbone.Collection = function(options) {
-			var argsPlusInjectedArgs = depsFromArgs(this, arguments);
-
-			// Act like nothing happened, eh tbranyen.
-			CollectionConstructor.apply(this, argsPlusInjectedArgs);
-		};
-
-		// Copy over the prototype as well.
-		Backbone.Collection.prototype = CollectionConstructor.prototype;
-
-		// Monkey-patch `extend()`
-		Backbone.Collection.extend = function(protoProps, staticProps) {
-			annotate(protoProps.initialize);
-			return CollectionConstructor.extend.call(this, protoProps, staticProps);
-		};
-
-	}
-
-	Provider.Widget = Widget;
-	Provider.infect = infect;
-
-	//Provider.annotate = annotate;
-	//Provider.deps = depsFromArgs;
 
 	// AMD / RequireJS
   if ( typeof define !== 'undefined' && define.amd ) {
